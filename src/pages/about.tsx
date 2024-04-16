a06189074870224ef9a6633c36dac8009ef9a988
Manuel Santos (CTW) <manuel.rs.santos@ctw.bmwgroup.com>
2024-04-16 03:12:19 +0100

    testing functions

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,16 @@
+import { useEffect, useState } from "react";
+
 export default function About() {
+  const [message, setMessage] = useState("");
+  useEffect(() => {
+    fetch("/api/helloworld")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
   return (
     <>
       <h1>ABOUT</h1>
+      <p>{message}</p>
     </>
   );
 }
