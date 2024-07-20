5b26b489e35360ff23e915bbc2e665484d92c680
Manuel Santos (CTW) <manuel.rs.santos@ctw.bmwgroup.com>
2024-07-20 17:42:05 +0100

    fix suspense on login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, {Suspense} from 'react'
 import Layout from '@/components/Layout'
 import Login from '@/components/Login';
 
 const LoginPage = () => {
   return (
     <Layout>
+        <Suspense>
       <Login />
+        </Suspense>
     </Layout>
   )
 }
