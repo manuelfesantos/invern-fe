cb19db9c821daa55f48c2dfe6759fd4b7d70a010
Manuel Santos (CTW) <manuel.rs.santos@ctw.bmwgroup.com>
2024-08-04 18:04:46 +0100

    fix lint and compile errors

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,72 +1,95 @@
-import Layout from '@/components/Layout';
-import React from 'react'
-import Image from 'next/image';
-import About1 from '../../assets/about1.jpeg';
-import About2 from '../../assets/about2.jpeg';
-import About3 from '../../assets/about3.jpeg';
-import Background from '@/components/Background';
+import Layout from "@/components/Layout";
+import React from "react";
+import Image from "next/image";
+import About1 from "../../assets/about1.jpeg";
+import About2 from "../../assets/about2.jpeg";
 
 const About = () => {
-    return (
-        <Layout>
-          <section className="flex flex-col flex-grow justify-center">
-            <div className='flex'>
-              <div className="flex-1 px-12 lg:px-40 flex flex-col justify-center gap-6 mt-12">
-                <div className='flex flex-col items-center'>
-                  <h2>Hey! I'm Mafalda,</h2>
-                  <h3 className='hidden lg:block'>And Welcome To My Creative Space.</h3>
-                  <h4 className='lg:hidden'>And Welcome To My Creative Space.</h4>
-                </div>
-                <hr />
-                <Image src={About1} alt="" className='w-full h-24 lg:h-48 object-cover' />
-                <div className='flex w-full items-center'>
-                  <h3 className='hidden lg:block'>I'm so excited to be starting my online business as a ceramics artist!</h3>
-                  <h4 className='lg:hidden'>I'm so excited to be starting my online business as a ceramics artist!</h4>
-                </div>
-                <p>
-                I've always been drawn to the mysterious and eerie beauty of Nordic landscapes,
-                and I love incorporating those elements into my work. The dark colors and shadows add an extra layer of depth and intrigue to each piece.
-                </p>
-                <p>
-                As a child, I was always fascinated by the textures and shapes of broken objects, and that's something I try to capture in my ceramics. The subtle 
-                imperfections and worn edges give each piece a unique character, don't you think? And speaking of character, I adore adding old traces and patina to my 
-                work – it gives it a sense of history and story.
-                </p>
-                <div className='flex flex-col lg:flex-row gap-12'>
-                  <div className='flex-1'>
-                    <h4>
-                    In fact, my background in scenography and interior design has been incredibly influential on my ceramics.
-                    </h4>
-                    <Image src={About2} alt="" className='lg:hidden w-full h-[420px] object-cover'/>
-                    <br />
-                    <p>
-                    I love thinking about the spaces where people will display or use these pieces – how they'll be lit, what colors will surround them, and how they'll interact with the other objects in the room. It's
-                    all about creating a sense of atmosphere and mood.
-                    </p>
-                    <br />
-                    <p>
-                    As I venture into the architecture world, I'm excited to bring my visual sensibilities and passion for texture and form to bear on larger-scale 
-                    projects. Who knows – maybe one day I'll design a building that incorporates some of those same broken, worn textures into its walls or furniture!
-                    </p>
-                    <br />
-                    <h4>
-                    For now, though, it's all about building my online presence as a ceramics artist.
-                    </h4>
-                    <br />
-                    <h3>Wish me luck!</h3>
-                    <br />
-                    <p>
-                    What do you think about my work? Would love to hear your
-                    feedback and see what resonates with you.
-                    </p>
-                  </div>
-                  <Image src={About2} alt="" className='hidden lg:block w-[40%] h-[560px]  object-cover'/>
-                </div>
-              </div>
+  return (
+    <Layout>
+      <section className="flex flex-col flex-grow justify-center">
+        <div className="flex">
+          <div className="flex-1 px-12 lg:px-40 flex flex-col justify-center gap-6 mt-12">
+            <div className="flex flex-col items-center">
+              <h2>{"Hey! I'm Mafalda,"}</h2>
+              <h3 className="hidden lg:block">
+                And Welcome To My Creative Space.
+              </h3>
+              <h4 className="lg:hidden">And Welcome To My Creative Space.</h4>
             </div>
-          </section>
-        </Layout>
-      );
-}
+            <hr />
+            <Image
+              src={About1}
+              alt=""
+              className="w-full h-24 lg:h-48 object-cover"
+            />
+            <div className="flex w-full items-center">
+              <h3 className="hidden lg:block">
+                {
+                  "I'm so excited to be starting my online business as a ceramics artist!"
+                }
+              </h3>
+              <h4 className="lg:hidden">
+                {
+                  "I'm so excited to be starting my online business as a ceramics artist!"
+                }
+              </h4>
+            </div>
+            <p>
+              {`I've always been drawn to the mysterious and eerie beauty of Nordic landscapes,
+                    and I love incorporating those elements into my work. The dark colors and shadows add an extra layer of depth and intrigue to each piece.`}
+            </p>
+            <p>
+              {`As a child, I was always fascinated by the textures and shapes of broken objects, and that's something I try to capture in my ceramics. The subtle 
+                    imperfections and worn edges give each piece a unique character, don't you think? And speaking of character, I adore adding old traces and patina to my 
+                    work – it gives it a sense of history and story.`}
+            </p>
+            <div className="flex flex-col lg:flex-row gap-12">
+              <div className="flex-1">
+                <h4>
+                  In fact, my background in scenography and interior design has
+                  been incredibly influential on my ceramics.
+                </h4>
+                <Image
+                  src={About2}
+                  alt=""
+                  className="lg:hidden w-full h-[420px] object-cover"
+                />
+                <br />
+                <p>
+                  {`I love thinking about the spaces where people will display or use these pieces – how they'll be lit, what colors will surround them, and how they'll interact with the other objects in the room. It's
+                        all about creating a sense of atmosphere and mood.`}
+                </p>
+                <br />
+                <p>
+                  {`As I venture into the architecture world, I'm excited to bring my visual sensibilities and passion for texture and form to bear on larger-scale 
+                        projects. Who knows – maybe one day I'll design a building that incorporates some of those same broken, worn textures into its walls or furniture!`}
+                </p>
+                <br />
+                <h4>
+                  {
+                    "For now, though, it's all about building my online presence as a ceramics artist."
+                  }
+                </h4>
+                <br />
+                <h3>Wish me luck!</h3>
+                <br />
+                <p>
+                  What do you think about my work? Would love to hear your
+                  feedback and see what resonates with you.
+                </p>
+              </div>
+              <Image
+                src={About2}
+                alt=""
+                className="hidden lg:block w-[40%] h-[560px]  object-cover"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
+};
 
-export default About
+export default About;
--- src/components/ProductCarousel2.tsx
+++ src/components/ProductCarousel2.tsx
@@ -1,116 +1,172 @@
-'use client'
-import { IProductDetails } from '@/types/store/product'
-import React, { useState, useEffect, useRef, SetStateAction } from "react";
+"use client";
+import { IProductDetails } from "@/types/store/product";
+import React, { useState, useEffect, useRef } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-const ProductCarousel2 = ({product}:{product:IProductDetails}) => {
-    const [nav1, setNav1] = useState(null);
-    const [nav2, setNav2] = useState(null);
-    let sliderRef1 = useRef(null);
-    let sliderRef2 = useRef(null);
+const NextArrow = (props: any) => {
+  const { className, onClick, style } = props;
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+      style={{
+        ...style,
+        position: "absolute",
+        top: "50%",
+        right: "0",
+        zIndex: 20,
+        width: "2.5rem",
+        height: "2.5rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    />
+  );
+};
 
-    useEffect(() => {
-      setNav1(sliderRef1);
-      setNav2(sliderRef2);
-    }, []);
+const PrevArrow = (props: any) => {
+  const { className, onClick, style } = props;
+  return (
+    <div
+      className={className}
+      onClick={onClick}
+      style={{
+        ...style,
+        position: "absolute",
+        top: "50%",
+        left: "0",
+        zIndex: 20,
+        width: "2.5rem",
+        height: "2.5rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    />
+  );
+};
+
+const ProductCarousel2 = ({ product }: { product: IProductDetails }) => {
+  const [nav1, setNav1] = useState<Slider | null>(null);
+  const [nav2, setNav2] = useState<Slider | null>(null);
+  const [nav3, setNav3] = useState<Slider | null>(null);
+  const [nav4, setNav4] = useState<Slider | null>(null);
+  let sliderRef1 = useRef<Slider | null>(null);
+  let sliderRef2 = useRef<Slider | null>(null);
+  let sliderRef3 = useRef<Slider | null>(null);
+  let sliderRef4 = useRef<Slider | null>(null);
+
+  useEffect(() => {
+    // @ts-ignore
+    setNav1(sliderRef1);
+    // @ts-ignore
+    setNav2(sliderRef2);
+    // @ts-ignore
+    setNav3(sliderRef3);
+    // @ts-ignore
+    setNav4(sliderRef4);
+  }, []);
 
   return (
     <>
-        <div className="hidden lg:grid grid-cols-5 gap-2">
-            <div className='col-span-1'>
-                <Slider
-                    asNavFor={nav1}
-                    ref={slider => (sliderRef2 = slider)}
-                    slidesToShow={product.images.length}
-                    swipeToSlide={true}
-                    focusOnSelect={true}
-                    arrows={false}
-                    vertical={true}
-                    verticalSwiping={true}>
-                        {
-                            product?.images.map((item,index) => (
-                                <div key={index}>
-                                    <div className='img-body'>
-                                        <img
-                                            src={item.url}
-                                            alt={item.alt}
-                                            />
-                                    </div>
-                                </div>
-                            ))
-                        }
-                </Slider>
-            </div>
-            <div className='col-span-4'>
-                <Slider
-                    asNavFor={nav2}
-                    ref={slider => (sliderRef1 = slider)}
-                    vertical={true}
-                    verticalSwiping={true}
-                    arrows={false}>
-                        {
-                            product?.images.map((item,index) => (
-                                <div key={index}>
-                                    <div className='img-body'>
-                                        <img
-                                            src={item.url}
-                                            alt={item.alt}
-                                            />
-                                    </div>
-                                </div>
-                            ))
-                        }
-                </Slider>
-            </div>
+      <div className="hidden lg:grid grid-cols-5 gap-2">
+        <div className="col-span-1">
+          {
+            //@ts-ignore
+            <Slider
+              asNavFor={nav1}
+              //@ts-ignore
+              ref={(slider) => (sliderRef2 = slider)}
+              slidesToShow={product.images.length}
+              swipeToSlide={true}
+              focusOnSelect={true}
+              arrows={false}
+              vertical={true}
+            >
+              {product?.images.map((item, index) => (
+                <div key={index}>
+                  <div className="img-body">
+                    <img
+                      src={item.url}
+                      alt={item.alt}
+                      className={"cursor-pointer"}
+                    />
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          }
         </div>
-        <div className="lg:hidden">
-            <div className=''>
-                <Slider
-                    asNavFor={nav2}
-                    ref={slider => (sliderRef1 = slider)}
-                    arrows={false}>
-                        {
-                            product?.images.map((item,index) => (
-                                <div key={index}>
-                                    <div className='img-body'>
-                                        <img
-                                            src={item.url}
-                                            alt={item.alt}
-                                            />
-                                    </div>
-                                </div>
-                            ))
-                        }
-                </Slider>
-            </div>
-            <div className=''>
-                <Slider
-                    asNavFor={nav1}
-                    ref={slider => (sliderRef2 = slider)}
-                    slidesToShow={product.images.length}
-                    swipeToSlide={true}
-                    focusOnSelect={true}
-                    arrows={false}>
-                        {
-                            product?.images.map((item,index) => (
-                                <div key={index}>
-                                    <div className='img-body'>
-                                        <img
-                                            src={item.url}
-                                            alt={item.alt}
-                                            />
-                                    </div>
-                                </div>
-                            ))
-                        }
-                </Slider>
-            </div>
+        <div className="col-span-4">
+          {
+            //@ts-ignore
+            <Slider
+              asNavFor={nav2}
+              //@ts-ignore
+              ref={(slider) => (sliderRef1 = slider)}
+              arrows={true}
+              nextArrow={<NextArrow />}
+              prevArrow={<PrevArrow />}
+            >
+              {product?.images.map((item, index) => (
+                <div key={index}>
+                  <div className="img-body">
+                    <img src={item.url} alt={item.alt} />
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          }
         </div>
+      </div>
+      <div className="lg:hidden">
+        <div className="">
+          {
+            //@ts-ignore
+            <Slider
+              asNavFor={nav4}
+              //@ts-ignore
+              ref={(slider) => (sliderRef3 = slider)}
+              arrows={false}
+            >
+              {product?.images.map((item, index) => (
+                <div key={index}>
+                  <div className="img-body">
+                    <img src={item.url} alt={item.alt} />
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          }
+        </div>
+        <div className="">
+          {
+            //@ts-ignore
+            <Slider
+              asNavFor={nav3}
+              //@ts-ignore
+              ref={(slider) => (sliderRef4 = slider)}
+              slidesToShow={product.images.length}
+              swipeToSlide={true}
+              focusOnSelect={true}
+              arrows={false}
+            >
+              {product?.images.map((item, index) => (
+                <div key={index}>
+                  <div className="img-body">
+                    <img src={item.url} alt={item.alt} />
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          }
+        </div>
+      </div>
     </>
+  );
+};
 
-  )
-}
-
-export default ProductCarousel2
+export default ProductCarousel2;
