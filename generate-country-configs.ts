92511c751f72238a213bd612ea0593bdac81edcd
Manuel Santos (CTW) <manuel.rs.santos@ctw.bmwgroup.com>
2024-09-12 22:55:47 +0100

    debug getCountryData

--- generate-country-configs.ts
+++ generate-country-configs.ts
@@ -2,11 +2,15 @@ import * as fs from "fs";
 
 const getCountryConfigs = () => {
   fetch("https://preview.invern-be.pages.dev/country/all")
-    .then((response) =>
+    .then((response) => {
+      console.log(
+        "getting response from server. Response code: ",
+        response.status,
+      );
       response.json().then((data) => {
         fs.writeFileSync("public/data.json", JSON.stringify(data.data));
-      }),
-    )
+      });
+    })
     .catch((err) => {
       throw err;
     });
