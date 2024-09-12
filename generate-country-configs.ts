2592444882620c456b1f1ce053cc46e9214d59b9
Manuel Santos (CTW) <manuel.rs.santos@ctw.bmwgroup.com>
2024-09-12 23:03:19 +0100

    fix getCountryData request headers

--- generate-country-configs.ts
+++ generate-country-configs.ts
@@ -1,14 +1,19 @@
 import * as fs from "fs";
 
 const getCountryConfigs = () => {
-  fetch("https://preview.invern-be.pages.dev/country/all")
+  fetch("https://preview.invern-be.pages.dev/country/all", {
+    headers: {
+      "CF-Access-Client-Id": "9a316892e7496497c4d7ac97e20a05c0.access",
+      "CF-Access-Client-Secret":
+        "a08859efde27988e755b742783ca4160b90bef8d494812a4df4f00b453a0b7c9",
+    },
+  })
     .then((response) => {
       console.log(
         "getting response from server. Response code: ",
         response.status,
       );
       response.json().then((data) => {
-        console.log("data:", data.data);
         fs.writeFileSync("public/data.json", JSON.stringify(data.data));
       });
     })
